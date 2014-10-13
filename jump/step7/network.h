3b3d1757eb7aa84a77cd146dc453a7811d64af23
lwch <lwch748@gmail.com>
2014-10-14 00:14:34 +0800

    step6 & step7

--- jump/step7/network.h
+++ jump/step7/network.h
@@ -36,7 +36,6 @@ extern network_t network;
 
 extern int bind_and_listen(unsigned short port);
 extern int connect_server(char* ip, unsigned short port);
-extern void reply_echo(int fd, struct iphdr* ipHdr);
 extern void server_loop(int remotefd, int localfd);
 extern ssize_t read_n(int fd, void* buf, size_t count);
 extern ssize_t write_n(int fd, const void* buf, size_t count);
