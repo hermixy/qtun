aa12937511484125dafe0cb39876b6f3350fe90a
lwch <lwch748@gmail.com>
2015-03-22 22:54:41 +0800

    删除旧代码

--- src_old/lib/common.h
+++
@@ -1,7 +0,0 @@
-#ifndef _COMMON_H_
-#define _COMMON_H_
-
-extern double microtime();
-extern long long microtime64();
-
-#endif
--- src_old/lib/ip_package.h
+++
@@ -1,8 +0,0 @@
-#ifndef _IP_PACKAGE_H_
-#define _IP_PACKAGE_H_
-
-#include <stdlib.h>
-
-extern int read_ip_package(int fd, int have_type, void* buffer, size_t* len);
-
-#endif
